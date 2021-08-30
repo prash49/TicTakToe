8a1221b04b06e85b88fbd3a6d7cf76814cd80877
Prashanth <imprash4u@gmail.com>
2021-08-30 09:53:22 +0530

    Added:Player Chosing X or O

--- src/com/bridgelabz/tictaktoe/TicTakToeGame.java
+++ src/com/bridgelabz/tictaktoe/TicTakToeGame.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.tictaktoe;
 
+import java.util.Scanner;
+
 public class TicTakToeGame {
      static char[] board=new char[10];
     public static void ticTakToeInitialization(){
@@ -7,8 +9,23 @@ public class TicTakToeGame {
             board[i]=' ';
         }
     }
+     static void playerChose(){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Player choose X or O");
+        char playerChoose=scanner.next().charAt(0);
+        if(playerChoose== 'X' || playerChoose == 'x'){
+            System.out.println("Player Chosen X");
+            System.out.println("Computer Choice O");
+        }else {
+            System.out.println("Player Chosen O");
+            System.out.println("computer Choice X ");
+        }
+
+
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tak Toe Game");
         ticTakToeInitialization();
+        playerChose();
     }
 }
