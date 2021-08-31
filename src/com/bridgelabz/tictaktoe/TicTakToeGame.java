bc9f5355697b5ccaae9c1fa7ea79c4bfca2d4fa8
Prashanth <imprash4u@gmail.com>
2021-08-31 17:26:52 +0530

    Added:Tossing Before To Check who plays First

--- src/com/bridgelabz/tictaktoe/TicTakToeGame.java
+++ src/com/bridgelabz/tictaktoe/TicTakToeGame.java
@@ -1,10 +1,12 @@
 package com.bridgelabz.tictaktoe;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class TicTakToeGame {
     static char[] board = new char[10];
     static Scanner scanner = new Scanner(System.in);
+    static Random random = new Random();
     public static char playerChoice;
 
     private static void ticTakToeInitialization() {
@@ -13,9 +15,19 @@ public class TicTakToeGame {
         }
     }
 
+    private static void tossToPlay() {
+        System.out.println("Tossing To Check who Plays First");
+        int toss = random.nextInt(2);
+        if (toss == 0) {
+            System.out.println("Its Heads Player Plays first");
+        } else {
+            System.out.println("Its Tails Computer Plays first");
+        }
+    }
+
     private static void playerChose() {
         System.out.println("Player choose X or O");
-         playerChoice = scanner.next().charAt(0);
+        playerChoice = scanner.next().charAt(0);
         if (playerChoice == 'X' || playerChoice == 'x') {
             System.out.println("Player Chosen :" + playerChoice);
             System.out.println("Computer Choice O");
@@ -27,11 +39,11 @@ public class TicTakToeGame {
     }
 
     private static void showBoard() {
-        System.out.println(board[1] + "   | " + board[2] + " | " + board[3]);
-        System.out.println("  --|---|---");
+        System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
+        System.out.println("-----------");
         System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
-        System.out.println("  --|---|----");
-        System.out.println(board[7] + "   | " + board[8] + " | " + board[9]);
+        System.out.println("-----------");
+        System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
     }
 
     private static void playerMove() {
@@ -40,23 +52,20 @@ public class TicTakToeGame {
         if (indexNumber < 1 || indexNumber > 9) {
             System.out.println("You Entered Invalid Position ");
             playerMove();
-        }else if(board[indexNumber] !=' ') {
+        } else if (board[indexNumber] != ' ') {
             System.out.println("The Index already occupied kindly Chose other index " +
                     "by observing Game Board ");
             playerMove();
-        }
-        else {
+        } else {
             board[indexNumber] = playerChoice;
             showBoard();
         }
-
     }
 
-
-
     public static void main(String[] args) {
-        System.out.println("Welcome to Tic Tak Toe Game");
+        System.out.println("_______XOX______________Welcome to Tic Tak Toe Game_________OXO________________");
         ticTakToeInitialization();
+        tossToPlay();
         playerChose();
         showBoard();
         playerMove();
